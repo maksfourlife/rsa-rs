7ed7bd520d6758e82accf96acc26f6f1e36b7944
maksfourlife <maksfourlife@gmail.com>
2022-12-10 12:02:41 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,45 @@
 use crate::rsa::{PrivKey, PubKey};
 use clap::Parser;
 use cli::{Cli, Commands};
+use serde::{de::DeserializeOwned, Serialize};
 use std::io;
 
 pub mod cli;
 pub mod rsa;
+
+fn base64_decode<T: DeserializeOwned>(input: impl AsRef<[u8]>) -> anyhow::Result<T> {
+    Ok(bincode::deserialize(&base64::decode(input)?)?)
+}
+
+fn base64_encode<T: Serialize + ?Sized>(data: &T) -> anyhow::Result<String> {
+    Ok(base64::encode(bincode::serialize(data)?))
+}
+
+fn read_line(message: impl AsRef<str>) -> io::Result<String> {
+    println!("{}", message.as_ref());
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    Ok(input)
+}
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     match cli.command {
         Commands::Generate { bits } => {
             let (pubkey, privkey) = rsa::gen_keypair(bits);
-            println!(
-                "===== PUBLIC KEY ====\n{}",
-                base64::encode(&bincode::serialize(&pubkey)?)
-            );
-            println!(
-                "==== PRIVATE KEY ====\n{}",
-                base64::encode(&bincode::serialize(&privkey)?)
-            );
+            println!("Pubkey:\n{}", base64_encode(&pubkey)?);
+            println!("Privkey:\n{}", base64_encode(&privkey)?);
         }
         Commands::Encrypt => {
-            let mut pubkey = String::new();
-            println!("Pubkey:");
-            io::stdin().read_line(&mut pubkey)?;
-            let pubkey: PubKey =
-                bincode::deserialize(&base64::decode(&pubkey[..pubkey.len() - 1])?)?;
-
-            let mut input = String::new();
-            println!("Message:");
-            io::stdin().read_line(&mut input)?;
-
-            let cipher_text = base64::encode(pubkey.encrypt(&input[..input.len() - 1]));
-            println!("Cypher text: {cipher_text}");
+            let pubkey: PubKey = base64_decode(read_line("Pubkey: ")?.trim_end())?;
+            let message = read_line("Message: ")?;
+            let cypher_text = base64::encode(pubkey.encrypt(&message));
+            println!("Cypher text: {cypher_text}");
         }
         Commands::Decrypt => {
-            let mut privkey = String::new();
-            println!("Privkey: ");
-            io::stdin().read_line(&mut privkey)?;
-            let privkey: PrivKey =
-                bincode::deserialize(&base64::decode(&privkey[..privkey.len() - 1])?)?;
-
-            let mut input = String::new();
-            println!("Cypher text:");
-            io::stdin().read_line(&mut input)?;
-
-            let message =
-                String::from_utf8(privkey.decrypt(&base64::decode(&input[..input.len() - 1])?))?;
+            let privkey: PrivKey = base64_decode(read_line("Privkey: ")?.trim_end())?;
+            let cypher_text = base64::decode(read_line("Cypher text:")?.trim_end())?;
+            let message = String::from_utf8(privkey.decrypt(&cypher_text))?;
             println!("Message: {message}");
         }
     }
