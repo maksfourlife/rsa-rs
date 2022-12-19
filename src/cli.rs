2c87dd41ed4d193fe9dc98e23f75352dedaea7b2
maksfourlife <maksfourlife@gmail.com>
2022-12-19 17:39:49 +0100

    now works with files
    approach about encodings:
    encrypt:
    	message from stdin/file => no encoding
    	cipher to stdout => base64
    	cipher to file => no encoding
    decrypt:
    	cipher from stdin => base64
    	cipher from file => no encoding
    	message to stdout/file => no encoding
    keypair is always to stdout

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,34 @@
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 
 #[derive(Debug, Parser)]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Commands,
+    /// program input, pass "-" for stdin
+    #[clap(long, default_value_t = String::from("-"))]
+    pub input: String,
+    /// program output, pass "-" for stdout
+    #[clap(long, default_value_t = String::from("-"))]
+    pub output: String,
+}
+
+#[derive(Debug, Default, Clone, Copy, ValueEnum)]
+pub enum Encoding {
+    #[default]
+    NoEncoding,
+    Base64,
+    Hex,
+}
+
+impl Encoding {
+    /// input is usually a string
+    pub fn decode<T: AsRef<[u8]>>(&self, input: T) -> anyhow::Result<Vec<u8>> {
+        Ok(match self {
+            Self::NoEncoding => input.as_ref().to_vec(),
+            Self::Base64 => base64::decode(input)?,
+            Self::Hex => hex::decode(input)?,
+        })
+    }
 }
 
 #[derive(Debug, Subcommand)]
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use crate::rsa::{PrivKey, PubKey};
+use crate::rsa::PubKey;
 use clap::Parser;
 use cli::{Cli, Commands};
+use rsa::PrivKey;
 use serde::{de::DeserializeOwned, Serialize};
-use std::io;
+use std::{
+    fs::File,
+    io::{self, Read, Write},
+};
 
 pub mod cli;
 pub mod rsa;
@@ -15,33 +19,85 @@ fn base64_encode<T: Serialize + ?Sized>(data: &T) -> anyhow::Result<String> {
     Ok(base64::encode(bincode::serialize(data)?))
 }
 
-fn read_line(message: impl AsRef<str>) -> io::Result<String> {
-    println!("{}", message.as_ref());
-    let mut input = String::new();
-    io::stdin().read_line(&mut input)?;
-    Ok(input)
+#[inline]
+fn process_generate(bits: usize) -> anyhow::Result<()> {
+    let (pubkey, privkey) = rsa::gen_keypair(bits);
+    println!(
+        r"-----BEGIN RSA PRIVATE KEY-----
+{}
+-----END RSA PRIVATE KEY-----
+-----BEGIN PUBLIC KEY-----
+{}
+-----END PUBLIC KEY-----
+",
+        base64_encode(&pubkey)?,
+        base64_encode(&privkey)?
+    );
+    Ok(())
+}
+
+/// If path='-' reads line from standard input, else from file
+fn read(path: &str) -> io::Result<Vec<u8>> {
+    Ok(match path {
+        "-" => read_line()?.as_bytes().to_vec(),
+        _ => {
+            let mut buf = vec![];
+            File::open(path)?.read_to_end(&mut buf)?;
+            buf
+        }
+    })
+}
+
+fn write(path: &str, data: &[u8]) -> io::Result<()> {
+    match path {
+        "-" => io::stdout().write_all(data),
+        _ => File::create(path)?.write_all(data),
+    }
+}
+
+/// Reads line from standard input and trims '\n'
+#[inline]
+fn read_line() -> io::Result<String> {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf)?;
+    Ok(buf[..buf.len() - 1].to_string())
+}
+
+#[inline]
+fn process_encrypt(input: &str, output: &str) -> anyhow::Result<()> {
+    let pubkey: PubKey = base64_decode(read_line()?)?;
+    // text in file and stdin is not encoded
+    let message = read(input)?;
+    let cipher = pubkey.encrypt(message);
+    match output {
+        "-" => println!("{}", base64::encode(cipher)),
+        _ => File::create(output)?.write_all(&cipher)?,
+    };
+    Ok(())
+}
+
+#[inline]
+fn process_decrypt(input: &str, output: &str) -> anyhow::Result<()> {
+    let privkey: PrivKey = base64_decode(read_line()?)?;
+    let cipher = match input {
+        "-" => base64_decode(read_line()?)?,
+        _ => {
+            let mut buf = vec![];
+            File::open(input)?.read_to_end(&mut buf)?;
+            buf
+        }
+    };
+    let message = privkey.decrypt(&cipher);
+    write(output, &message)?;
+    Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     match cli.command {
-        Commands::Generate { bits } => {
-            let (pubkey, privkey) = rsa::gen_keypair(bits);
-            println!("Pubkey:\n{}", base64_encode(&pubkey)?);
-            println!("Privkey:\n{}", base64_encode(&privkey)?);
-        }
-        Commands::Encrypt => {
-            let pubkey: PubKey = base64_decode(read_line("Pubkey: ")?.trim_end())?;
-            let message = read_line("Message: ")?;
-            let cypher_text = base64::encode(pubkey.encrypt(&message));
-            println!("Cypher text: {cypher_text}");
-        }
-        Commands::Decrypt => {
-            let privkey: PrivKey = base64_decode(read_line("Privkey: ")?.trim_end())?;
-            let cypher_text = base64::decode(read_line("Cypher text:")?.trim_end())?;
-            let message = String::from_utf8(privkey.decrypt(&cypher_text))?;
-            println!("Message: {message}");
-        }
+        Commands::Generate { bits } => process_generate(bits)?,
+        Commands::Encrypt => process_encrypt(&cli.input, &cli.output)?,
+        Commands::Decrypt => process_decrypt(&cli.input, &cli.output)?,
     }
     Ok(())
 }
